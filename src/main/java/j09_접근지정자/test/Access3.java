c5771ad0a2cb30c73dd1394bca66e319ad7de058
geon00 <jgn159753@naver.com>
2022-11-17 12:55:21 +0900

    챕터9 출력

---
+++ src/main/java/j09_접근지정자/test/Access3.java
@@ -0,0 +1,10 @@
+package j09_접근지정자.test;
+
+public class Access3 {
+     private String address;
+
+    private void printAddress() {
+
+        System.out.println("address: " + this.address);
+    }
+}
---
+++ src/main/java/j09_접근지정자/캡슐화/StudentMain.java
@@ -0,0 +1,10 @@
+package j09_접근지정자.캡슐화;
+
+public class StudentMain {
+    public static void main(String[] args) {
+
+        Student s1 = new Student("장건녕", "남");
+
+        s1.printInfo();
+    }
+}
