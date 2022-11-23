4a554e8b54f9e77a7ff3c71ce7b83fbee1cfd7fa
geon00 <jgn159753@naver.com>
2022-11-23 13:47:10 +0900

    챕터11 출력

--- src/main/java/j11_상속/동물/AnimalMain.java
+++ src/main/java/j11_상속/동물/AnimalMain.java
@@ -12,6 +12,8 @@ public class AnimalMain {
         Human human = new Human();
         Tiger tiger = new Tiger();
 
+
+
         Animal hAnimal = human; // 업캐스팅
         Animal tAnimal = tiger;
         Animal[] animals = new Animal[3];
@@ -20,10 +22,13 @@ public class AnimalMain {
         animals[1] = new Tiger();
         animals[2] = new Animal();
 
+
+        System.out.println("[업캐스팅]");
         for (int i = 0; i < animals.length; i++) {
             animals[i].move(); // 반복문을 통해 move가 실행된다.
         }
 
+        System.out.println("[다운캐스팅]");
         for (int i = 0; i < animals.length; i++) {
             if (animals[i] instanceof Human) {
                 Human h = (Human) animals[0]; // 명시적 형변환
